6a493d0eaf964a3b77dd2427803b08f23b36835d
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-08-31 12:45:39 -0400

    bug fixed

--- frontend/src/components/NewArrivals.jsx
+++ frontend/src/components/NewArrivals.jsx
@@ -1,15 +1,33 @@
 import ProductItem from './ProductItem'
+import React, { useEffect, useState } from 'react'
+import { appFirebase } from '../config/firebase.jsx'
+import { getFirestore, collection, addDoc, getDocs, query, limit } from 'firebase/firestore'
 
 export default function NewArrivals() {
-  const newArrivals = [1]
-  const uniqueArrivals = [...new Set(newArrivals)]
-  return (
-    <div className="d-flex flex-wrap">
-      {uniqueArrivals.map((newArr) => (
-        <div key={newArr} className="col-md-3 col-xs-6">
-          <ProductItem />
-        </div>
-      ))}
+  const db = getFirestore(appFirebase)
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'products'), query(limit(3)))
+        const productList = []
+        querySnapshot.forEach(doc => {
+          productList.push({ ...doc.data(), id: doc.id })
+        })
+        setProducts(productList)
+      } catch (error) {
+        console.error('Error fetching products:', error)
+      }
+    }
+
+    fetchProducts()
+  }, [db])
+
+  return products.map((product, i) => (
+    <div className="col-md-3 col-xs-6">
+      <ProductItem {...product} />
     </div>
-  )
+  ))
 }
