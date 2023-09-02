5c87eddcc19df5ce26eb70c257aaef7d93e3c630
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-09-02 07:16:47 -0400

    sales update

--- frontend/src/app/(store)/my-account/Orders.jsx
+++ frontend/src/app/(store)/my-account/Orders.jsx
@@ -1,35 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import salesService from '@/services/sales'
+import { useFetch } from '@/hooks/useFetch';
+import { formatter } from '@/lib/utils';
 
 const Orders = () => {
-  const [cartItems, setCartItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-
-    axios.get('http://localhost:3003/cart')
-      .then((response) => {
-        const cartItemsData = response.data;
-        setCartItems(cartItemsData);
-        setLoading(false); 
-      })
-      .catch((error) => {
-        console.error('Error al obtener elementos del carrito:', error);
-        setError(error); 
-        setLoading(false); 
-      });
-  }, []);
-
-  if (loading) {
-    return <div>Cargando...</div>; 
-  }
+  const { data: cartItems, error, isLoading } =  useFetch(salesService.getAllSales())
 
   if (error) {
     return <div>Error: {error.message}</div>; 
   }
+  
+  if (isLoading) {
+    return <div>Wait a minute, loading data</div>
+  }
 
-
+  console.log(cartItems[0].productos)
   return (
     <div className="table-wrap mb-30">
       <table className="shop_table cart table">
@@ -48,30 +35,30 @@ const Orders = () => {
             <tr key={item._id} className=" cart_item">
               <td className="product-thumbnail">
                 <a href="#">
-                  <img src={`URL_DE_TU_IMAGEN/${item.productId}`} alt="" />
+                  <small>product image</small>
                 </a>
               </td>
               <td className="product-name">
-                <a href="#">Nombre del producto</a>
+                <a href="#">Product</a>
                 <ul>
                   <li>Size: XL</li>
                   <li>Color: White</li>
                 </ul>
               </td>
               <td className="product-price">
-                <span className="amount"> {item.price ? `$${item.price.toFixed(2)}` : 'N/A'}</span>
+                <span className="amount"> {formatter.format(item.precioConIva / item?.productos[0].cantidad) }</span>
               </td>
               <td className="product-quantity">
-                <span className="amount">{item.quantity} pcs</span>
+                <span className="amount">{item?.productos[0].cantidad} pcs</span>
               </td>
               <td className="product-subtotal">
-                <span className="amount">{item.price ? `$${item.price.toFixed(2)}` : 'N/A'}</span>
+                <span className="amount">{formatter.format(item.precioConIva)}</span>
               </td>
-              <td className="product-remove">
-                <a href="#" className="remove" title="Remove this item">
+              {/* <td className="product-remove">
+                <a className="remove" title="Remove this item">
                   <i className="icon icon_close" />
                 </a>
-              </td>
+              </td */}
             </tr>
           ))}
         </tbody>
